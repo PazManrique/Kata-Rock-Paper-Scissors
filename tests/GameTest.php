37c2da9d6a368193635d3ecf2373b359b80ecb63
Paz Manrique <nautilus1985@gmail.com>
2022-08-24 09:11:15 +0200

    refactoring the ifs

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -90,6 +90,45 @@ public function test_player1_rock_player2_paper_2_win()
     $this->assertEquals("Player 2 wins", $winner);
 }
 
+public function test_player1_rock_player2_rock_draw()
+{
+    $game = new Game;
+    $player1 = new Player;
+    $player2 = new Player;
+
+    $player1->choose("Rock");
+    $player2->choose("Rock");
+    $winner=$game->start($player1, $player2);
+
+    $this->assertEquals("It's a draw", $winner);
+}
+
+public function test_player1_scissors_player2_scissors_draw()
+{
+    $game = new Game;
+    $player1 = new Player;
+    $player2 = new Player;
+
+    $player1->choose("Scissors");
+    $player2->choose("Scissors");
+    $winner=$game->start($player1, $player2);
+
+    $this->assertEquals("It's a draw", $winner);
+}
+
+public function test_player1_paper_player2_paper_draw()
+{
+    $game = new Game;
+    $player1 = new Player;
+    $player2 = new Player;
+
+    $player1->choose("Rock");
+    $player2->choose("Rock");
+    $winner=$game->start($player1, $player2);
+
+    $this->assertEquals("It's a draw", $winner);
+}
+
     
     
 }
